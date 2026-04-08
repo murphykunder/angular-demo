88534af4f14f245adc2fe089fdfe3ffae9c97a7c
Alisha Kunder <alisha.kunder.29@gmail.com>
2026-04-09 00:23:59 +0530

    fix table

--- src/app/shared/directives/resizable-column-directive.directive.ts
+++ src/app/shared/directives/resizable-column-directive.directive.ts
@@ -14,6 +14,7 @@ export class ResizableColumnDirectiveDirective implements OnInit {
   private startWidth = 0;
   private handle!: HTMLElement;
   private isResizing = false;
+  private minColWidthPx = 50;
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
@@ -21,7 +22,6 @@ export class ResizableColumnDirectiveDirective implements OnInit {
     this.handle = this.renderer.createElement('span');
     this.handle.className = 'resize-handle';
     this.renderer.appendChild(this.el.nativeElement, this.handle);
-
     this.renderer.listen(this.handle, 'mousedown', this.onMouseDown);
   }
 
@@ -29,7 +29,7 @@ export class ResizableColumnDirectiveDirective implements OnInit {
     event.preventDefault();
     event.stopPropagation();
     this.isResizing = true;
-    this.resizingChange.emit(true); // notify parent
+    this.resizingChange.emit(true);
     this.startX = event.pageX;
     this.startWidth = this.el.nativeElement.getBoundingClientRect().width;
 
@@ -45,7 +45,7 @@ export class ResizableColumnDirectiveDirective implements OnInit {
     const tableWidth = table.getBoundingClientRect().width;
 
     let newWidthPx = this.startWidth + dx;
-    if (newWidthPx < 50) newWidthPx = 50;
+    if (newWidthPx < this.minColWidthPx) newWidthPx = this.minColWidthPx;
 
     const newWidthPercent = (newWidthPx / tableWidth) * 100;
     this.resize.emit({ colIndex: this.colIndex, newWidthPercent });
@@ -56,7 +56,7 @@ export class ResizableColumnDirectiveDirective implements OnInit {
     document.removeEventListener('mouseup', this.onMouseUp);
     setTimeout(() => {
       this.isResizing = false;
-      this.resizingChange.emit(false); // notify parent
+      this.resizingChange.emit(false);
     }, 0);
 
   };
